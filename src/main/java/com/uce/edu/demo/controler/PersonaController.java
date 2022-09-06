5a6d43fe821059177c91f09da62020caa75d416c
Christian Moya <cmoyacharro@gamil.com>
2022-09-05 22:49:15 -0500

    TALLER 38: Programación Web - GET, POST, PUT y DELETE

--- src/main/java/com/uce/edu/demo/controler/PersonaController.java
+++ src/main/java/com/uce/edu/demo/controler/PersonaController.java
@@ -5,7 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.uce.edu.demo.modelo.Persona;
@@ -25,4 +29,37 @@ public class PersonaController {
 		return "vistaListaEstudiantes";
 	}
  
+	@GetMapping("/buscarUno/{idPersona}")
+	public String buscarPersona(@PathVariable("idPersona") Integer id, Model modelo) {
+		System.out.println("el id " + id);
+		Persona per = this.personaService.buscarPorId(id);
+		modelo.addAttribute("persona",per);
+		return "vistaPersona";
+	}
+	
+	@PutMapping("/actualizar/{idPersona}")
+	public String actualizarPersona(@PathVariable("idPersona") Integer id, Persona persona) {
+		persona.setId(id);
+		this.personaService.actualizar(persona);
+		return "redirect:/personas/buscar";
+	}
+	
+	@DeleteMapping("/borrar/{idPersona}")
+	public String borrarPersona(@PathVariable("idPersona") Integer id, Persona persona) {
+		this.personaService.eliminar(id);
+		return "redirect:/personas/buscar";
+	}
+	
+	@PostMapping("/insertar")
+	public String insertarPersona(Persona persona) {
+		this.personaService.insertar(persona);
+		return "redirect:/personas/buscar";
+	}
+	
+	//Metodos de redireccionamineto a paginas 
+	@GetMapping("/nuevaPersona")
+	public String paginaNuevaPersona(Persona persona){
+			return "vistaNuevaPersona";
+
+		}
 }
